d3b2176375319079893ef60fdda48fbb1136cf84
William Da Silva <arcanianarcher@gmail.com>
2018-01-28 23:48:02 -0500

    Add comp1406/fetchMarks utility

---
+++ comp1406/fetchMarks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+'''
+Utility to aid uploading marks. It traverses the contents of
+assignmentDirectory, and whenever it finds a mark file, it moves the contents of
+the mark file into the user's clipboard so that they can paste it on CULearn.
+
+This utility is intended to be a stopgap until I get fully automatic uploading
+to CULearn working.
+'''
+
+import argparse
+import os
+
+try:
+    import pyperclip
+except ModuleNotFoundError as error:
+    print(error)
+    print('This utility requires pyperclip to function.')
+    exit()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('assignmentDirectory', type=str,
+    help='path to the directory containing all of the marked assignments')
+args = parser.parse_args()
+for root, dirs, files in os.walk(args.assignmentDirectory):
+    if 'mark' in files:
+        print(root, end='')
+        with open(os.path.join(root, 'mark'), 'r') as markFile:
+            pyperclip.copy(markFile.read())
+        input() # wait until user presses the enter key
