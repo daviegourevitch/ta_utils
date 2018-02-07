2333e200d5147c20ff6f5e7ab64922f23b4f57d7
William Da Silva <arcanianarcher@gmail.com>
2018-02-06 19:20:46 -0500

    Add makeMarkFiles.py

---
+++ makeMarkFiles.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+'''
+unzips all of the zip files under targetDirectory into a directory with
+the same name as the zip file, in the same directory as the zip file.
+'''
+
+import os
+from shutil import copyfile
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def makeMarkFiles(targetDirectory, templateFile, markFileName):
+    for root, dirs, files in os.walk(targetDirectory):
+        for dirname in dirs:
+            if dirname.endswith('_assignsubmission_file_'):
+                markFilePath = os.path.join(root, dirname, markFileName)
+                if templateFile is not None:
+                    copyfile(templateFile, markFilePath)
+                else:
+                    open(markFilePath, 'a+').close()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('targetDirectory', type=str, nargs='?',
+        default=dir_path,
+        help='path to the directory containing all of the zip files')
+    parser.add_argument('--templateFile', type=str, nargs='?',
+        default=None,
+        help='path to the marking template file')
+    parser.add_argument('--markFileName', type=str, nargs='?',
+        default='mark',
+        help='path to the marking template file')
+    args = parser.parse_args()
+    makeMarkFiles(args.targetDirectory, args.templateFile, args.markFileName)
